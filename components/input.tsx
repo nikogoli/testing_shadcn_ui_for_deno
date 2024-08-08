55eb61a46144faec68e43b90faa55944558b3df9
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-09 01:07:39 +0900

    追加：Input 対応

---
+++ components/input.tsx
@@ -0,0 +1,24 @@
+// @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
+import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
+
+import { cn } from '../modules/lib/utils.ts'
+
+
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ class:className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        'flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  )
+})
+Input.displayName = 'Input'
+
+export { Input }
