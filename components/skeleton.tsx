b3fe183980ac0c9cbb208deef2e50753edd503b1
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-09 10:22:51 +0900

    追加：Skeleton 対応

---
+++ components/skeleton.tsx
@@ -0,0 +1,7 @@
+import { cn } from '../modules/lib/utils.ts'
+
+function Skeleton({ class:className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />
+}
+
+export { Skeleton }
