06bbb5372004cb1b3dfac006e6871ce283578eb9
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-11 00:08:44 +0900

    変更(HvrCrd)：vendor 追加+整理+パス調整

--- components/hover-card.tsx
+++ components/hover-card.tsx
@@ -1,6 +1,6 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
-import * as HoverCardPrimitive from '@radix-ui/react-hover-card'
+import * as HoverCardPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-hover-card@1.0.7.js'
 import * as AltHoverCardPrimitive from "../modules/lib/components/hover-card.d.ts"
 
 import { cn } from '../modules/lib/utils.ts'
