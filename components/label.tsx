2cc20e9b9f242a802a679da1a3162722cdb045aa
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-10 20:15:33 +0900

    変更(Label)：vendor 追加+整理+パス調整

--- components/label.tsx
+++ components/label.tsx
@@ -1,8 +1,8 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
-import * as LabelPrimitive from '@radix-ui/react-label'
+import * as LabelPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-label@2.0.2.js'
 import * as AltLabelPrimitive from "../modules/lib/components/label.d.ts"
-import { VariantProps, cva } from 'class-variance-authority'
+import { VariantProps, cva } from '../modules/esm.sh/class-variance-authority@0.7.0.js'
 
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
