4d42d9bd799e566cd8cc7fa6188d11ea5e93f92f
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-10 17:40:59 +0900

    追加(Clpsbl)：開閉状態に合わせて hidden を設定する class

--- components/collapsible.tsx
+++ components/collapsible.tsx
@@ -1,9 +1,16 @@
+// @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
+import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
 import * as CollapsiblePrimitive from "../modules/esm.sh/v132/_@radix-ui/react-collapsible@1.0.3.js"
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+function CollapsibleContent(props:React.ComponentProps<"div">){
+  const {class:classStr, ...others} = props
+  return(
+    <CollapsiblePrimitive.CollapsibleContent class={`data-[state=closed]:hidden ${classStr}`} {...others} />
+  )
+}
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
