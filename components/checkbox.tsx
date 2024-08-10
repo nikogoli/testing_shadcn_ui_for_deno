009fc112db16a7c951db0a3202fc78a8183037e1
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-10 16:23:14 +0900

    変更(Chckbx)：vendor 追加+整理+パス調整

--- components/checkbox.tsx
+++ components/checkbox.tsx
@@ -1,6 +1,6 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import * as CheckboxPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-checkbox@1.0.4.js'
 import * as AltCheckboxPrimitive from "../modules/lib/components/checkbox.d.ts"
 
 import { cn } from '../modules/lib/utils.ts'
