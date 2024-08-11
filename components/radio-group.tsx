37d00249799af2f716dbb7e8e8045f9530ae841b
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-11 16:28:38 +0900

    追加(Rdo)：コンポーネント

---
+++ components/radio-group.tsx
@@ -0,0 +1,44 @@
+// @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
+import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
+import * as RadioGroupPrimitive from "../modules/esm.sh/_@radix-ui/react-radio-group@1.1.3.js"
+import * as AltRadioGroupPrimitive from "../modules/lib/components/radio-group.d.ts"
+
+import { cn } from "../modules/lib/utils.ts"
+import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
+
+const RadioGroup = React.forwardRef<
+  ElementRef<typeof AltRadioGroupPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof AltRadioGroupPrimitive.Root>
+>(({ class:className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Root
+      className={cn("grid gap-2", className)}
+      {...props}
+      ref={ref}
+    />
+  )
+})
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
+
+const RadioGroupItem = React.forwardRef<
+  ElementRef<typeof AltRadioGroupPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof AltRadioGroupPrimitive.Item>
+>(({ class:className, children, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        "h-4 w-4 rounded-full border border-input ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      {...props}
+    >
+      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+        <span class="i-tabler:circle-filled flex h-2.5 w-2.5 fill-primary text-primary" />
+      </RadioGroupPrimitive.Indicator>
+    </RadioGroupPrimitive.Item>
+  )
+})
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
+
+export { RadioGroup, RadioGroupItem }
