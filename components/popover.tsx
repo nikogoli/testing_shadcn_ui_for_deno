0ab44beb602522fe303a94ee108f72021032e1e2
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-11 12:46:01 +0900

    変更(Popovr)：vendor 追加+整理+パス調整

--- components/popover.tsx
+++ components/popover.tsx
@@ -1,6 +1,6 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
-import * as PopoverPrimitive from '@radix-ui/react-popover'
+import * as PopoverPrimitive from '../modules/esm.sh/_@radix-ui/react-popover@1.0.7.js'
 import * as AltPopoverPrimitive from "../modules/lib/components/popover.d.ts"
 
 import { cn } from '../modules/lib/utils.ts'
