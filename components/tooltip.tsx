d68d8d8e0fbfc25def218112863c3c3faf1f9bc9
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-12 00:04:56 +0900

    追加(Tltip)：コンポーネント

---
+++ components/tooltip.tsx
@@ -0,0 +1,31 @@
+// @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
+import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
+import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import * as AlitTooltipPrimitive from "../modules/lib/components/tooltip.d.ts"
+
+import { cn } from '../modules/lib/utils.ts'
+import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
+
+const TooltipProvider = TooltipPrimitive.Provider
+
+const Tooltip = TooltipPrimitive.Root
+
+const TooltipTrigger = TooltipPrimitive.Trigger
+
+const TooltipContent = React.forwardRef<
+  ElementRef<typeof AlitTooltipPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof AlitTooltipPrimitive.Content>
+>(({ class:className, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Content
+    ref={ref}
+    sideOffset={sideOffset}
+    className={cn(
+      'z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1',
+      className
+    )}
+    {...props}
+  />
+))
+TooltipContent.displayName = TooltipPrimitive.Content.displayName
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
