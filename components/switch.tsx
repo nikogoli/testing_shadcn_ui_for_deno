09eb9f6c1db0ee485f8e1ae696130711058109b1
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-11 19:47:54 +0900

    追加(Swch)：コンポーネント

---
+++ components/switch.tsx
@@ -0,0 +1,30 @@
+// @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
+import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
+import * as SwitchPrimitives from '@radix-ui/react-switch'
+import * as AltSwitchPrimitives from "../modules/lib/components/switch.d.ts"
+
+import { cn } from '../modules/lib/utils.ts'
+import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
+
+const Switch = React.forwardRef<
+  ElementRef<typeof AltSwitchPrimitives.Root>,
+  ComponentPropsWithoutRef<typeof AltSwitchPrimitives.Root>
+>(({ class:className, ...props }, ref) => (
+  <SwitchPrimitives.Root
+    className={cn(
+      'peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+      className
+    )}
+    {...props}
+    ref={ref}
+  >
+    <SwitchPrimitives.Thumb
+      className={cn(
+        'pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0'
+      )}
+    />
+  </SwitchPrimitives.Root>
+))
+Switch.displayName = SwitchPrimitives.Root.displayName
+
+export { Switch }
