09dff3c6c5fde89be5f371867892ad3ff3b6de5d
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-21 01:18:16 +0900

    変更(Input)：class の整理

--- components/input.tsx
+++ components/input.tsx
@@ -4,14 +4,19 @@ import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
 import { cn } from '../modules/lib/utils.ts'
 
 
-export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {}
+interface InputProps extends React.HTMLAttributes<HTMLInputElement> {}
+
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(({ class:className, type, ...props }, ref) => {
   return (
     <input
       type={type}
       className={cn(
-        'flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        `flex h-10 w-full rounded-md border border-input bg-background px-3 py-2
+         text-sm ring-offset-background
+         file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground
+         focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2
+         disabled:cursor-not-allowed disabled:opacity-50`,
         className
       )}
       ref={ref}
@@ -20,5 +25,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ class:className,
   )
 })
 Input.displayName = 'Input'
+
 
 export { Input }
