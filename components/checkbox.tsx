56b21518ccc5eed2f7dd2f7e722954cac5f75a7a
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-20 12:32:21 +0900

    変更(ChckBx)：class の整理

--- components/checkbox.tsx
+++ components/checkbox.tsx
@@ -2,6 +2,7 @@
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
 import * as CheckboxPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-checkbox@1.0.4.js'
 import * as AltCheckboxPrimitive from "../modules/lib/components/checkbox.d.ts"
+import IconCheck from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/check.tsx"
 
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
@@ -13,13 +14,13 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'peer h-4 w-4 shrink-0 rounded-sm border border-input ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+      'peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
-      <span className='i-lucide:check flex h-4 w-4' />
+      <IconCheck class='h-4 w-4' />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
