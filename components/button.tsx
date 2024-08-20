4c36f57a95e5e2831d3b422479cf16d1d555006d
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-20 12:02:04 +0900

    変更(Btn)：class の整理 + variant 設定の切り出し&整理

--- components/button.tsx
+++ components/button.tsx
@@ -4,49 +4,57 @@ import { VariantProps, cva } from "../modules/esm.sh/class-variance-authority@0.
 
 import { cn } from "../modules/lib/utils.ts"
 
-const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background transition-transform active:scale-97",
-  {
-    variants: {
-      variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "underline-offset-4 hover:underline text-primary",
-      },
-      size: {
-        default: "h-10 py-2 px-4",
-        sm: "h-9 px-3 rounded-md",
-        lg: "h-11 px-8 rounded-md",
-      },
+
+const Variants = {
+  variants: {
+    variant: {
+      default: "bg-primary text-primary-foreground hover:bg-primary/80",
+      destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/80",
+      outline: "border border-input hover:bg-accent hover:text-accent-foreground",
+      secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/60",
+      ghost: "hover:bg-accent hover:text-accent-foreground",
+      link: "underline-offset-4 hover:underline text-primary",
+      noColorStyle: '',
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: "h-10 py-2 px-4",
+      sm: "h-9 px-3 rounded-md",
+      lg: "h-11 px-8 rounded-md",
     },
-  }
+  },
+  defaultVariants: {
+    variant: "default" as const,
+    size: "default" as const,
+  },
+}
+
+const classnamesByVariants = cva(
+  `inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium
+    focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50`,
+  { ...Variants}
 )
 
-export interface ButtonProps
+
+interface ButtonProps
   extends Omit<React.HTMLAttributes<HTMLButtonElement>, "size">,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof classnamesByVariants> {}
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ class:className, variant, size, ...props }, ref) => {
-    return (
-      <button
-        class={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
+
+const Button = React.forwardRef<
+  HTMLButtonElement, ButtonProps
+>(({ class:className, variant, size, ...props }, ref) => {
+  return (
+    <button
+      class={cn(classnamesByVariants({ variant, size, className }))}
+      ref={ref}
+      {...props}
+    />
+  )
+})
 Button.displayName = "Button"
 
-export { Button, buttonVariants }
+
+export {
+  Button,
+  Variants as buttonVariants
+}
