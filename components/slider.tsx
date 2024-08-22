bfee10942430831430b7a298ddfda3b175724880
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-22 19:12:30 +0900

    変更(Slider)：class の整理

--- components/slider.tsx
+++ components/slider.tsx
@@ -6,6 +6,7 @@ import * as AltSliderPrimitive from "../modules/lib/components/slider.d.ts"
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
 
+
 const Slider = React.forwardRef<
   ElementRef<typeof AltSliderPrimitive.Root>,
   ComponentPropsWithoutRef<typeof AltSliderPrimitive.Root>
@@ -15,12 +16,19 @@ const Slider = React.forwardRef<
     className={cn('relative flex w-full touch-none select-none items-center', className)}
     {...props}
   >
-    <SliderPrimitive.Track class="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range class="absolute h-full bg-primary" />
+    <SliderPrimitive.Track
+      className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+      <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb class="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors hover:cursor-grab active:cursor-grabbing focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb
+      className="block h-5 w-5 rounded-full border-2 border-primary bg-background
+       ring-offset-background transition-colors hover:cursor-grab active:cursor-grabbing
+       focus-visible:outline-none focus-visible:ring-2
+       focus-visible:ring-ring focus-visible:ring-offset-2
+       disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
+
 
 export { Slider }
