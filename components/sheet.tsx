2c6e8d1ba22ca363680660291392c0fbdb00b34b
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-22 19:11:17 +0900

    変更(Sheet)：不要な変更を revert しオリジナルに合わせるように + variant 切り出し + class 整理
    fix sheet

--- components/sheet.tsx
+++ components/sheet.tsx
@@ -2,36 +2,54 @@
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
 import * as SheetPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-dialog@1.0.5.js'
 import * as AltSheetPrimitive from "../modules/lib/components/dialog.d.ts"
+import IconX from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/x.tsx"
 
 import { VariantProps, cva } from '../modules/esm.sh/class-variance-authority@0.7.0.js'
 
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
 
-const Sheet = SheetPrimitive.Root
 
-const SheetTrigger = SheetPrimitive.Trigger
-
-const portalVariants = cva('fixed inset-0 z-50 flex', {
+const Variants = {
   variants: {
-    position: {
-      top: 'items-start',
-      bottom: 'items-end',
-      left: 'justify-start',
-      right: 'justify-end',
+    side: {
+      top: `inset-x-0 top-0 border-b
+        data-[state=open]:animate-slidein-fromtopfull
+        data-[state=closed]:animate-slideout-totopfull`,
+      bottom: `inset-x-0 bottom-0 border-t
+        data-[state=open]:animate-slidein-frombottomfull
+        data-[state=closed]:animate-slideout-tobottomfull`,
+      left: `inset-y-0 left-0 h-full w-3/4 border-r sm:max-w-sm
+        data-[state=open]:animate-slidein-fromleftfull
+        data-[state=closed]:animate-slideout-toleftfull`,
+      right: `inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm
+        data-[state=open]:animate-slidein-fromrightfull
+        data-[state=closed]:animate-slideout-torightfull`,
     },
   },
-  defaultVariants: { position: 'right' },
-})
+  defaultVariants: {
+    side: "right" as const,
+  },
+}
 
-interface SheetPortalProps extends AltSheetPrimitive.DialogPortalProps, VariantProps<typeof portalVariants> {}
-
-const SheetPortal = ({ position, class:className, children, ...props }: SheetPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props}>
-    <div className={portalVariants({ position })}>{children}</div>
-  </SheetPrimitive.Portal>
+const classnamesByVariants = cva(
+  `fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out
+   data-[state=closed]:[animation-duration:.3s] data-[state=open]:[animation-duration:.5s]`,
+  { ...Variants }
 )
-SheetPortal.displayName = SheetPrimitive.Portal.displayName
+
+
+const Sheet = SheetPrimitive.Root
+ 
+
+const SheetTrigger = SheetPrimitive.Trigger
+ 
+
+const SheetClose = SheetPrimitive.Close
+ 
+
+const SheetPortal = SheetPrimitive.Portal
+
 
 const SheetOverlay = React.forwardRef<
   ElementRef<typeof AltSheetPrimitive.Overlay>,
@@ -39,7 +57,8 @@ const SheetOverlay = React.forwardRef<
 >(({ class:className, children, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      'fixed inset-0 z-50 bg-black/80 backdrop-blur-sm transition-all duration-100 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in',
+      `fixed inset-0 z-50 bg-black/80
+       data-[state=open]:animate-in data-[state=closed]:animate-out`,
       className
     )}
     {...props}
@@ -48,104 +67,30 @@ const SheetOverlay = React.forwardRef<
 ))
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
-const sheetVariants = cva('fixed z-50 scale-100 gap-4 bg-background p-6 opacity-100 shadow-lg border', {
-  variants: {
-    position: {
-      top: 'animate-in slide-in-from-top w-full duration-300',
-      bottom: 'animate-in slide-in-from-bottom w-full duration-300',
-      left: 'animate-in slide-in-from-left h-full duration-300',
-      right: 'animate-in slide-in-from-right h-full duration-300',
-    },
-    size: {
-      content: '',
-      default: '',
-      sm: '',
-      lg: '',
-      xl: '',
-      full: '',
-    },
-  },
-  compoundVariants: [
-    {
-      position: ['top', 'bottom'],
-      size: 'content',
-      class: 'max-h-screen',
-    },
-    {
-      position: ['top', 'bottom'],
-      size: 'default',
-      class: 'h-1/3',
-    },
-    {
-      position: ['top', 'bottom'],
-      size: 'sm',
-      class: 'h-1/4',
-    },
-    {
-      position: ['top', 'bottom'],
-      size: 'lg',
-      class: 'h-1/2',
-    },
-    {
-      position: ['top', 'bottom'],
-      size: 'xl',
-      class: 'h-5/6',
-    },
-    {
-      position: ['top', 'bottom'],
-      size: 'full',
-      class: 'h-screen',
-    },
-    {
-      position: ['right', 'left'],
-      size: 'content',
-      class: 'max-w-screen',
-    },
-    {
-      position: ['right', 'left'],
-      size: 'default',
-      class: 'w-1/3',
-    },
-    {
-      position: ['right', 'left'],
-      size: 'sm',
-      class: 'w-1/4',
-    },
-    {
-      position: ['right', 'left'],
-      size: 'lg',
-      class: 'w-1/2',
-    },
-    {
-      position: ['right', 'left'],
-      size: 'xl',
-      class: 'w-5/6',
-    },
-    {
-      position: ['right', 'left'],
-      size: 'full',
-      class: 'w-screen',
-    },
-  ],
-  defaultVariants: {
-    position: 'right',
-    size: 'default',
-  },
-})
 
-export interface DialogContentProps
+interface DialogContentProps
   extends Omit<ComponentPropsWithoutRef<typeof AltSheetPrimitive.Content>, "size">,
-    VariantProps<typeof sheetVariants> {}
+    VariantProps<typeof classnamesByVariants> {}
 
-const SheetContent = React.forwardRef<ElementRef<typeof AltSheetPrimitive.Content>, DialogContentProps>(
-  ({ position, size, class:className, children, ...props }, ref) => (
-    <SheetPortal position={position}>
+
+const SheetContent = React.forwardRef<
+  ElementRef<typeof AltSheetPrimitive.Content>,
+  DialogContentProps
+>(
+  ({ side="right", class:className, children, ...props }, ref) => (
+    <SheetPortal>
       <SheetOverlay />
-      <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ position, size }), className)} {...props}>
+      <SheetPrimitive.Content
+        ref={ref}
+        className={cn(classnamesByVariants({ side }), className)} {...props}>
         {children}
-        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-          <span className="i-lucide:x flex h-4 w-4"></span>
-          <span className="sr-only">Close</span>
+        <SheetPrimitive.Close
+            className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background
+             transition-opacity hover:opacity-100
+             focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2
+             disabled:pointer-events-none data-[state=open]:bg-secondary">
+          <IconX class="h-4 w-4" />
+          <span class="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
     </SheetPortal>
@@ -153,30 +98,57 @@ const SheetContent = React.forwardRef<ElementRef<typeof AltSheetPrimitive.Conten
 )
 SheetContent.displayName = SheetPrimitive.Content.displayName
 
+
 const SheetHeader = ({ class:className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn('flex flex-col space-y-2 text-center sm:text-left', className)} {...props} />
+  <div
+    class={cn('flex flex-col space-y-2 text-center sm:text-left', className)}
+    {...props} />
 )
 SheetHeader.displayName = 'SheetHeader'
 
+
 const SheetFooter = ({ class:className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />
+  <div
+    class={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)}
+    {...props} />
 )
 SheetFooter.displayName = 'SheetFooter'
+
 
 const SheetTitle = React.forwardRef<
   ElementRef<typeof AltSheetPrimitive.Title>,
   ComponentPropsWithoutRef<typeof AltSheetPrimitive.Title>
 >(({ class:className, ...props }, ref) => (
-  <SheetPrimitive.Title ref={ref} className={cn('text-lg font-semibold text-foreground', className)} {...props} />
+  <SheetPrimitive.Title
+    ref={ref}
+    className={cn('text-lg font-semibold text-foreground', className)}
+    {...props} />
 ))
 SheetTitle.displayName = SheetPrimitive.Title.displayName
+
 
 const SheetDescription = React.forwardRef<
   ElementRef<typeof AltSheetPrimitive.Description>,
   ComponentPropsWithoutRef<typeof AltSheetPrimitive.Description>
 >(({ class:className, ...props }, ref) => (
-  <SheetPrimitive.Description ref={ref} className={cn('text-sm text-muted-foreground', className)} {...props} />
+  <SheetPrimitive.Description
+    ref={ref}
+    className={cn('text-sm text-muted-foreground', className)}
+    {...props} />
 ))
 SheetDescription.displayName = SheetPrimitive.Description.displayName
 
-export { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetFooter, SheetTitle, SheetDescription }
+
+export {
+  Sheet,
+  SheetPortal,
+  SheetOverlay,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+  Variants as sheetVariatns
+}
