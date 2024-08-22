e62a05581feb1d77e7ecec8589b8f50c3a4bb224
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-23 04:45:54 +0900

    変更(Tab)：class の整理

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -6,7 +6,9 @@ import * as AltTabsPrimitive from "../modules/lib/components/tabs.d.ts"
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
 
+
 const Tabs = TabsPrimitive.Root
+
 
 const TabsList = React.forwardRef<
   ElementRef<typeof AltTabsPrimitive.List>,
@@ -23,6 +25,7 @@ const TabsList = React.forwardRef<
 ))
 TabsList.displayName = TabsPrimitive.List.displayName
 
+
 const TabsTrigger = React.forwardRef<
   ElementRef<typeof AltTabsPrimitive.Trigger>,
   ComponentPropsWithoutRef<typeof AltTabsPrimitive.Trigger>
@@ -30,13 +33,18 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     class={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+      `inline-flex items-center justify-center whitespace-nowrap rounded-sm
+       px-3 py-1.5 text-sm font-medium ring-offset-background transition-all
+       focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2
+       disabled:pointer-events-none disabled:opacity-50
+       data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm`,
       className
     )}
     {...props}
   />
 ))
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
+
 
 const TabsContent = React.forwardRef<
   ElementRef<typeof AltTabsPrimitive.Content>,
@@ -45,12 +53,14 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     class={cn(
-      'mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+      `mt-2 ring-offset-background
+       focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2`,
       className
     )}
     {...props}
   />
 ))
 TabsContent.displayName = TabsPrimitive.Content.displayName
+
 
 export { Tabs, TabsList, TabsTrigger, TabsContent }
