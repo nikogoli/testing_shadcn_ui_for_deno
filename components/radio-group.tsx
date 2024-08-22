dffc4c095542c8cb7ac213376b4d69eafd3ba791
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-22 19:08:49 +0900

    変更(RdoGrp)：class の整理

--- components/radio-group.tsx
+++ components/radio-group.tsx
@@ -2,9 +2,11 @@
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
 import * as RadioGroupPrimitive from "../modules/esm.sh/_@radix-ui/react-radio-group@1.1.3.js"
 import * as AltRadioGroupPrimitive from "../modules/lib/components/radio-group.d.ts"
+import IconCircleFilled from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/circle-filled.tsx"
 
 import { cn } from "../modules/lib/utils.ts"
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
+
 
 const RadioGroup = React.forwardRef<
   ElementRef<typeof AltRadioGroupPrimitive.Root>,
@@ -20,6 +22,7 @@ const RadioGroup = React.forwardRef<
 })
 RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 
+
 const RadioGroupItem = React.forwardRef<
   ElementRef<typeof AltRadioGroupPrimitive.Item>,
   ComponentPropsWithoutRef<typeof AltRadioGroupPrimitive.Item>
@@ -28,17 +31,22 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "h-4 w-4 rounded-full border border-input ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        `aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background
+         focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2
+         [&+label]:cursor-pointer
+         disabled:cursor-not-allowed disabled:opacity-50
+         disabled:[&+label]:cursor-not-allowed disabled:[&+label]:opacity-50`,
         className
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <span class="i-tabler:circle-filled flex h-2.5 w-2.5 fill-primary text-primary" />
+        <IconCircleFilled class="h-2.5 w-2.5 fill-current text-current" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
 })
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
+
 
 export { RadioGroup, RadioGroupItem }
