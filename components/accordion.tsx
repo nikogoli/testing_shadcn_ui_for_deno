224ca759643b5a787e7eb1f97cf18d40c99d87e0
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-24 20:11:08 +0900

    変更(Acrd)：型調整

--- components/accordion.tsx
+++ components/accordion.tsx
@@ -1,34 +1,40 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
+// @deno-types="../modules/lib/components/accordion.d.ts"
 import * as AccordionPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-accordion@1.1.2.js'
+import * as AccordionTypes from "../modules/lib/components/accordion.d.ts"
+
 import IconChevronDown from "https://deno.land/x/tabler_icons_tsx@0.0.7/tsx/chevron-down.tsx"
 
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
-import * as AltAccordionPrimitive from "../modules/lib/components/accordion.d.ts"
 
 
-const Accordion: React.FunctionComponent<(AccordionPrimitive.AccordionSingleProps | AccordionPrimitive.AccordionMultipleProps)&React.RefAttributes<HTMLDivElement>&{class:string}> = AccordionPrimitive.Root
+const Accordion = AccordionPrimitive.Root
 
 
 const AccordionItem = React.forwardRef<
-  ElementRef<typeof AltAccordionPrimitive.Item>,
-  ComponentPropsWithoutRef<typeof AltAccordionPrimitive.Item>
+  ElementRef<typeof AccordionTypes.Item>,
+  ComponentPropsWithoutRef<typeof AccordionTypes.Item>
 >(({ class:className, ...props }, ref) => (
-  <AccordionPrimitive.Item ref={ref} className={cn('border-b', className)} {...props} />
+  <AccordionPrimitive.Item
+    ref={ref}
+    className={cn('border-b', className)}
+    {...props} />
 ))
 AccordionItem.displayName = 'AccordionItem'
 
 
 const AccordionTrigger = React.forwardRef<
-  ElementRef<typeof AltAccordionPrimitive.Trigger>,
-  ComponentPropsWithoutRef<typeof AltAccordionPrimitive.Trigger>
+  ElementRef<typeof AccordionTypes.Trigger>,
+  ComponentPropsWithoutRef<typeof AccordionTypes.Trigger>
 >(({ class:className, children, ...props }, ref) => (
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+        `flex flex-1 items-center justify-between py-4 font-medium transition-all
+         hover:underline data-[state=open]:[&>svg]:rotate-180`,
         className
       )}
       {...props}
@@ -42,13 +48,15 @@ AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
 
 
 const AccordionContent = React.forwardRef<
-  ElementRef<typeof AltAccordionPrimitive.Content>,
-  ComponentPropsWithoutRef<typeof AltAccordionPrimitive.Content>
+  ElementRef<typeof AccordionTypes.Content>,
+  ComponentPropsWithoutRef<typeof AccordionTypes.Content>
 >(({ class:className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
     className={cn(
-      `overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down data-[state=closed]:hidden`,
+      `overflow-hidden text-sm transition-all
+       data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down
+       data-[state=closed]:hidden`,
       className
     )}
     {...props}
@@ -61,4 +69,9 @@ const AccordionContent = React.forwardRef<
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent
+}
