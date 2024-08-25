645760bdf833362f5e9b083a1756f5fd35c448d1
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-25 13:58:43 +0900

    変更(Avat)：型 調整

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,15 +1,16 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
+// @deno-types="../modules/lib/components/avatar.d.ts"
 import * as AvatarPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-avatar@1.0.4.js'
-import * as AltAvatarPrimitive from "../modules/lib/components/avatar.d.ts"
+import * as AvatarTypes from "../modules/lib/components/avatar.d.ts"
 
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
 
 
 const Avatar = React.forwardRef<
-  ElementRef<typeof AltAvatarPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof AltAvatarPrimitive.Root>
+  ElementRef<typeof AvatarTypes.Root>,
+  ComponentPropsWithoutRef<typeof AvatarTypes.Root>
 >(({ class:className, ...props }, ref) => (
   <AvatarPrimitive.Root
     ref={ref}
@@ -21,17 +22,20 @@ Avatar.displayName = AvatarPrimitive.Root.displayName
 
 
 const AvatarImage = React.forwardRef<
-  ElementRef<typeof AltAvatarPrimitive.Image>,
-  ComponentPropsWithoutRef<typeof AltAvatarPrimitive.Image>
+  ElementRef<typeof AvatarTypes.Image>,
+  ComponentPropsWithoutRef<typeof AvatarTypes.Image>
 >(({ class:className, ...props }, ref) => (
-  <AvatarPrimitive.Image ref={ref} className={cn('aspect-square h-full w-full', className)} {...props} />
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cn('aspect-square h-full w-full', className)}
+    {...props} />
 ))
 AvatarImage.displayName = AvatarPrimitive.Image.displayName
 
 
 const AvatarFallback = React.forwardRef<
-  ElementRef<typeof AltAvatarPrimitive.Fallback>,
-  ComponentPropsWithoutRef<typeof AltAvatarPrimitive.Fallback>
+  ElementRef<typeof AvatarTypes.Fallback>,
+  ComponentPropsWithoutRef<typeof AvatarTypes.Fallback>
 >(({ class:className, ...props }, ref) => (
   <AvatarPrimitive.Fallback
     ref={ref}
@@ -42,4 +46,8 @@ const AvatarFallback = React.forwardRef<
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
 
 
-export { Avatar, AvatarImage, AvatarFallback }
+export {
+  Avatar,
+  AvatarImage,
+  AvatarFallback
+}
--- modules/lib/components/avatar.d.ts
+++ modules/lib/components/avatar.d.ts
@@ -1,37 +1,38 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../../esm.sh/preact@10.19.6/compat.js'
-import { PrimitiveForwardRefComponent, ComponentPropsWithoutRef } from "../type-utils.ts"
-
-/**
- * Following type-definitions are based on "https://esm.sh/v132/@radix-ui/react-avatar@1.0.4/X-YS9AdHlwZXMvcmVhY3Q6cHJlYWN0L2NvbXBhdCxyZWFjdDpwcmVhY3QvY29tcGF0CmUvKg/dist/index.d.mts"
- */ 
+import { PrimitiveSpanProps, PrimitiveImageProps } from "../type-utils-Primitive.d.ts"
 
 
-type PrimitiveSpanProps = ComponentPropsWithoutRef<PrimitiveForwardRefComponent<"span">>;
-interface AvatarProps extends PrimitiveSpanProps {
-}
-
+/* -------------------------------------------------------------------------------------------------
+ * Root
+ * -----------------------------------------------------------------------------------------------*/
 export const Root: React.ForwardRefExoticComponent<
   AvatarProps & React.RefAttributes<HTMLSpanElement>
 >;
 
-
-
-interface AvatarFallbackProps extends PrimitiveSpanProps {
-  delayMs?: number;
+interface AvatarProps extends PrimitiveSpanProps {
 }
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Image
+ * -----------------------------------------------------------------------------------------------*/
+export const Image: React.ForwardRefExoticComponent<
+  AvatarImageProps & React.RefAttributes<HTMLImageElement>
+>;
+
+interface AvatarImageProps extends PrimitiveImageProps {
+    onLoadingStatusChange?: (status: 'idle' | 'loading' | 'loaded' | 'error') => void;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Root
+ * -----------------------------------------------------------------------------------------------*/
 export const Fallback: React.ForwardRefExoticComponent<
   AvatarFallbackProps & React.RefAttributes<HTMLSpanElement>
 >;
 
-
-
-type ImageLoadingStatus = 'idle' | 'loading' | 'loaded' | 'error';
-type PrimitiveImageProps = ComponentPropsWithoutRef<PrimitiveForwardRefComponent<"img">>;
-interface AvatarImageProps extends PrimitiveImageProps {
-    onLoadingStatusChange?: (status: ImageLoadingStatus) => void;
+interface AvatarFallbackProps extends PrimitiveSpanProps {
+  delayMs?: number;
 }
-
-export const Image: React.ForwardRefExoticComponent<
-  AvatarImageProps & React.RefAttributes<HTMLImageElement>
->;
