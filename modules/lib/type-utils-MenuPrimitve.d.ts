2758ef6dea2a2fa62504ae3b939edd75c2738643
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-25 16:05:54 +0900

    変更(Menu-Prim)：型情報 調整

--- modules/lib/type-utils-MenuPrimitve.d.ts
+++ modules/lib/type-utils-MenuPrimitve.d.ts
@@ -4,99 +4,75 @@ import { FocusScope } from "./type-utils-FocusScope.d.ts";
 import { DismissableLayer } from "./type-utils-DismissableLayer.d.ts";
 import * as RovingFocusGroup from "./type-utils-RovingFocus.d.ts"
 import * as PopperPrimitive from "./type-utils-PopperPrimitive.d.ts"
-import { PrimitiveForwardRefComponent, ComponentPropsWithoutRef } from "./type-utils.ts"
+import { PortalProps } from "./type-utils-PortalPrimitive.d.ts"; 
+
+import { ComponentPropsWithoutRef } from "./type-utils.ts"
+import { PrimitiveDivProps, PrimitiveSpanProps } from "./type-utils-Primitive.d.ts"
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Root
+ * -----------------------------------------------------------------------------------------------*/
+export const Root: React.FC<MenuProps>;
+
+interface MenuProps {
+    children?: React.ReactNode;
+    open?: boolean;
+    onOpenChange?(open: boolean): void;
+    dir?: 'ltr' | 'rtl';
+    modal?: boolean;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Anchor
+ * -----------------------------------------------------------------------------------------------*/
+export const Anchor: React.ForwardRefExoticComponent<MenuAnchorProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuAnchorProps extends PopperAnchorProps {
+}
+type PopperAnchorProps = ComponentPropsWithoutRef<typeof PopperPrimitive.Anchor>;
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Portal
+ * -----------------------------------------------------------------------------------------------*/
+export const Portal: React.FC<MenuPortalProps>;
+
+interface MenuPortalProps {
+    children?: React.ReactNode;
+    /**
+     * Specify a container element to portal the content into.
+     */
+    container?: PortalProps['container'];
+    /**
+     * Used to force mounting when more control is needed. Useful when
+     * controlling animation with React animation libraries.
+     */
+    forceMount?: true;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Content
+ * -----------------------------------------------------------------------------------------------*/
+export const Content: React.ForwardRefExoticComponent<MenuContentProps & React.RefAttributes<HTMLDivElement>>;
 
 /**
- * Following type-definitions are based on "https://esm.sh/v132/@radix-ui/react-menu@2.0.6/X-YS9AdHlwZXMvcmVhY3Q6cHJlYWN0L2NvbXBhdCxyZWFjdC1kb206cHJlYWN0L2NvbXBhdCxyZWFjdDpwcmVhY3QvY29tcGF0CmUvKg/dist/index.d.mts"
- */ 
-
-type PrimitiveDivProps = ComponentPropsWithoutRef<PrimitiveForwardRefComponent<"div">>;
-
-
-// -------- CheckboxItem -------------
-
-type CheckedState = boolean | 'indeterminate';
-// I ignore the below error...
-interface MenuCheckboxItemProps extends MenuItemProps {
-  checked?: CheckedState;
-  onCheckedChange?: (checked: boolean) => void;
+ * We purposefully don't union MenuRootContent and MenuSubContent props here because
+ * they have conflicting prop types. We agreed that we would allow MenuSubContent to
+ * accept props that it would just ignore.
+ */
+interface MenuContentProps extends MenuRootContentTypeProps {
+    /**
+     * Used to force mounting when more control is needed. Useful when
+     * controlling animation with React animation libraries.
+     */
+    forceMount?: true;
 }
-export const CheckboxItem: React.ForwardRefExoticComponent<
-  MenuCheckboxItemProps & React.RefAttributes<HTMLDivElement>
->;
-
-
-
-// deno-lint-ignore no-empty-interface
-interface MenuLabelProps extends PrimitiveDivProps {
-}
-export const Label: React.ForwardRefExoticComponent<
-  MenuLabelProps & React.RefAttributes<HTMLDivElement>
->;
-
-
-
-// -------- Item -------------
-
-interface MenuItemImplProps extends PrimitiveDivProps {
-  disabled?: boolean;
-  textValue?: string;
-}
-interface MenuItemProps extends Omit<MenuItemImplProps, 'onSelect'> {
-  onSelect?: (event: Event) => void;
+interface MenuRootContentTypeProps extends Omit<MenuContentImplProps, keyof MenuContentImplPrivateProps> {
 }
 
-export const Item: React.ForwardRefExoticComponent<
-  MenuItemProps & React.RefAttributes<HTMLDivElement>
->;
-
-
-
-
-// -------- RadioItem -------------
-
-interface MenuRadioItemProps extends MenuItemProps {
-  value: string;
-}
-export const RadioItem: React.ForwardRefExoticComponent<
-  MenuRadioItemProps & React.RefAttributes<HTMLDivElement>
->;
-
-
-
-
-// -------- Separator -------------
-
-// deno-lint-ignore no-empty-interface
-interface MenuSeparatorProps extends PrimitiveDivProps {
-}
-export const Separator: React.ForwardRefExoticComponent<
-  MenuSeparatorProps & React.RefAttributes<HTMLDivElement>
->;
-
-
-
-
-// -------- SubTrigger -------------
-
-interface MenuItemImplProps extends PrimitiveDivProps {
-  disabled?: boolean;
-  textValue?: string;
-}
-// deno-lint-ignore no-empty-interface
-interface MenuSubTriggerProps extends MenuItemImplProps {
-}
-
-export const SubTrigger: React.ForwardRefExoticComponent<
-  MenuSubTriggerProps & React.RefAttributes<HTMLDivElement>
->;
-
-
-
-// -------- Content -------------
-
-type FocusScopeProps = ComponentPropsWithoutRef<typeof FocusScope>;
-type DismissableLayerProps = ComponentPropsWithoutRef<typeof DismissableLayer>;
 type MenuContentImplPrivateProps = {
   onOpenAutoFocus?: FocusScopeProps['onMountAutoFocus'];
   onDismiss?: DismissableLayerProps['onDismiss'];
@@ -112,27 +88,6 @@ type MenuContentImplPrivateProps = {
    */
   trapFocus?: FocusScopeProps['trapped'];
 };
-// deno-lint-ignore no-empty-interface
-interface MenuRootContentTypeProps extends Omit<MenuContentImplProps, keyof MenuContentImplPrivateProps> {
-}
-interface MenuContentProps extends MenuRootContentTypeProps {
-  /**
-   * Used to force mounting when more control is needed. Useful when
-   * controlling animation with React animation libraries.
-   */
-  forceMount?: true;
-}
-
-export const Content: React.ForwardRefExoticComponent<
-  MenuContentProps & React.RefAttributes<HTMLDivElement>
->;
-
-
-
-// -------- SubContent -------------
-
-type RovingFocusGroupProps = ComponentPropsWithoutRef<typeof RovingFocusGroup.Root>;
-type PopperContentProps = ComponentPropsWithoutRef<typeof PopperPrimitive.Content>;
 interface MenuContentImplProps extends MenuContentImplPrivateProps, Omit<PopperContentProps, 'dir' | 'onPlaced'> {
   /**
    * Event handler called when auto-focusing on close.
@@ -151,24 +106,139 @@ interface MenuContentImplProps extends MenuContentImplPrivateProps, Omit<PopperC
   onInteractOutside?: DismissableLayerProps['onInteractOutside'];
 }
 
-// deno-lint-ignore no-empty-interface
-interface MenuRootContentTypeProps extends Omit<MenuContentImplProps, keyof MenuContentImplPrivateProps> {
-}
-interface MenuSubContentProps extends Omit<MenuContentImplProps, keyof MenuContentImplPrivateProps | 'onCloseAutoFocus' | 'onEntryFocus' | 'side' | 'align'> {
-  /**
-   * Used to force mounting when more control is needed. Useful when
-   * controlling animation with React animation libraries.
-   */
-  forceMount?: true;
-}
-
-export const SubContent: React.ForwardRefExoticComponent<
-  MenuSubContentProps & React.RefAttributes<HTMLDivElement>
->;
+type FocusScopeProps = ComponentPropsWithoutRef<typeof FocusScope>;
+type DismissableLayerProps = ComponentPropsWithoutRef<typeof DismissableLayer>;
+type RovingFocusGroupProps = ComponentPropsWithoutRef<typeof RovingFocusGroup.Root>;
+type PopperContentProps = ComponentPropsWithoutRef<typeof PopperPrimitive.Content>;
 
 
-// ------  Group ------------------
+/* -------------------------------------------------------------------------------------------------
+ * Group
+ * -----------------------------------------------------------------------------------------------*/
+export const Group: React.ForwardRefExoticComponent<MenuGroupProps & React.RefAttributes<HTMLDivElement>>;
 
 interface MenuGroupProps extends PrimitiveDivProps {
 }
-export const Group: React.ForwardRefExoticComponent<MenuGroupProps & React.RefAttributes<HTMLDivElement>>
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Label
+ * -----------------------------------------------------------------------------------------------*/
+export const Label: React.ForwardRefExoticComponent<MenuLabelProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuLabelProps extends PrimitiveDivProps {
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Item
+ * -----------------------------------------------------------------------------------------------*/
+export const Item: React.ForwardRefExoticComponent<MenuItemProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuItemProps extends Omit<MenuItemImplProps, 'onSelect'> {
+    onSelect?: (event: Event) => void;
+}
+interface MenuItemImplProps extends PrimitiveDivProps {
+    disabled?: boolean;
+    textValue?: string;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * CheckboxItem
+ * -----------------------------------------------------------------------------------------------*/
+export const CheckboxItem: React.ForwardRefExoticComponent<MenuCheckboxItemProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuCheckboxItemProps extends MenuItemProps {
+    checked?: boolean | 'indeterminate';
+    onCheckedChange?: (checked: boolean) => void;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * RadioGroup
+ * -----------------------------------------------------------------------------------------------*/
+export const RadioGroup: React.ForwardRefExoticComponent<MenuRadioGroupProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuRadioGroupProps extends MenuGroupProps {
+    value?: string;
+    onValueChange?: (value: string) => void;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * RadioItem
+ * -----------------------------------------------------------------------------------------------*/
+export const RadioItem: React.ForwardRefExoticComponent<MenuRadioItemProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuRadioItemProps extends MenuItemProps {
+    value: string;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * ItemIndicator
+ * -----------------------------------------------------------------------------------------------*/
+export const ItemIndicator: React.ForwardRefExoticComponent<MenuItemIndicatorProps & React.RefAttributes<HTMLSpanElement>>;
+
+interface MenuItemIndicatorProps extends PrimitiveSpanProps {
+    /**
+     * Used to force mounting when more control is needed. Useful when
+     * controlling animation with React animation libraries.
+     */
+    forceMount?: true;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Separator
+ * -----------------------------------------------------------------------------------------------*/
+export const Separator: React.ForwardRefExoticComponent<MenuSeparatorProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuSeparatorProps extends PrimitiveDivProps {
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Arrow
+ * -----------------------------------------------------------------------------------------------*/
+export const Arrow: React.ForwardRefExoticComponent<MenuArrowProps & React.RefAttributes<SVGSVGElement>>;
+
+interface MenuArrowProps extends PopperArrowProps {
+}
+type PopperArrowProps = ComponentPropsWithoutRef<typeof PopperPrimitive.Arrow>;
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Arrow
+ * -----------------------------------------------------------------------------------------------*/
+export const Sub: React.FC<MenuSubProps>;
+
+interface MenuSubProps {
+    children?: React.ReactNode;
+    open?: boolean;
+    onOpenChange?(open: boolean): void;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * SubTrigger
+ * -----------------------------------------------------------------------------------------------*/
+export const SubTrigger: React.ForwardRefExoticComponent<MenuSubTriggerProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuSubTriggerProps extends MenuItemImplProps {
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * SubContent
+ * -----------------------------------------------------------------------------------------------*/
+export const SubContent: React.ForwardRefExoticComponent<MenuSubContentProps & React.RefAttributes<HTMLDivElement>>;
+
+interface MenuSubContentProps extends Omit<MenuContentImplProps, keyof MenuContentImplPrivateProps | 'onCloseAutoFocus' | 'onEntryFocus' | 'side' | 'align'> {
+    /**
+     * Used to force mounting when more control is needed. Useful when
+     * controlling animation with React animation libraries.
+     */
+    forceMount?: true;
+}
