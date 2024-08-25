21331fd82b137ff97f1a0c2e502be4068634cd98
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-25 16:49:55 +0900

    変更(HovrCrd)：型 調整

--- components/hover-card.tsx
+++ components/hover-card.tsx
@@ -1,7 +1,8 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
+// @deno-types="../modules/lib/components/hover-card.d.ts"
 import * as HoverCardPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-hover-card@1.0.7.js'
-import * as AltHoverCardPrimitive from "../modules/lib/components/hover-card.d.ts"
+import * as HoverCardTypes from "../modules/lib/components/hover-card.d.ts"
 
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
@@ -14,8 +15,8 @@ const HoverCardTrigger = HoverCardPrimitive.Trigger
 
 
 const HoverCardContent = React.forwardRef<
-  ElementRef<typeof AltHoverCardPrimitive.Content>,
-  ComponentPropsWithoutRef<typeof AltHoverCardPrimitive.Content>
+  ElementRef<typeof HoverCardTypes.Content>,
+  ComponentPropsWithoutRef<typeof HoverCardTypes.Content>
 >(({ class:className, align = 'center', sideOffset = 4, ...props }, ref) => (
   <HoverCardPrimitive.Content
     ref={ref}
@@ -23,10 +24,10 @@ const HoverCardContent = React.forwardRef<
     sideOffset={sideOffset}
     className={cn(
       `z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none
-       data-[state=open]:data-[side=left]:animate-slidein-fromright
-       data-[state=open]:data-[side=right]:animate-slidein-fromleft
-       data-[state=open]:data-[side=top]:animate-slidein-frombottom
-       data-[state=open]:data-[side=bottom]:animate-slidein-fromtop
+       data-[state=open]:data-[side=left]:animate-slidein-from-right-50
+       data-[state=open]:data-[side=right]:animate-slidein-from-left-50
+       data-[state=open]:data-[side=top]:animate-slidein-from-bottom-50
+       data-[state=open]:data-[side=bottom]:animate-slidein-from-top-50
        data-[state=closed]:animate-zoomout-95`,
       className
     )}
--- modules/lib/components/hover-card.d.ts
+++ modules/lib/components/hover-card.d.ts
@@ -1,45 +1,96 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../../../modules/esm.sh/preact@10.19.6/compat.js'
+import { PortalProps } from "../type-utils-PortalPrimitive.d.ts"
 import { DismissableLayerProps } from "../type-utils-MenuPrimitve.d.ts";
-import { PopperContentProps } from "../type-utils-PopperPrimitive.d.ts"
+import { PopperContentProps, PopperArrowProps } from "../type-utils-PopperPrimitive.d.ts"
 
-/**
- * Following type-definitions are based on "https://esm.sh/v132/@radix-ui/react-hover-card@1.0.7/X-YS9AdHlwZXMvcmVhY3Q6cHJlYWN0L2NvbXBhdCxyZWFjdDpwcmVhY3QvY29tcGF0CmUvKg/dist/index.d.mts"
- */ 
+import { PrimitiveLinkProps } from "../type-utils-Primitive.d.ts"
 
 
-interface HoverCardContentImplProps extends Omit<PopperContentProps, 'onPlaced'> {
-  /**
-   * Event handler called when the escape key is down.
-   * Can be prevented.
-   */
-  onEscapeKeyDown?: DismissableLayerProps['onEscapeKeyDown'];
-  /**
-   * Event handler called when the a `pointerdown` event happens outside of the `HoverCard`.
-   * Can be prevented.
-   */
-  onPointerDownOutside?: DismissableLayerProps['onPointerDownOutside'];
-  /**
-   * Event handler called when the focus moves outside of the `HoverCard`.
-   * Can be prevented.
-   */
-  onFocusOutside?: DismissableLayerProps['onFocusOutside'];
-  /**
-   * Event handler called when an interaction happens outside the `HoverCard`.
-   * Specifically, when a `pointerdown` event happens outside or focus moves outside of it.
-   * Can be prevented.
-   */
-  onInteractOutside?: DismissableLayerProps['onInteractOutside'];
+/* -------------------------------------------------------------------------------------------------
+ * Root
+ * -----------------------------------------------------------------------------------------------*/
+export const Root: React.FC<HoverCardProps>;
+
+interface HoverCardProps {
+    children?: React.ReactNode;
+    open?: boolean;
+    defaultOpen?: boolean;
+    onOpenChange?: (open: boolean) => void;
+    openDelay?: number;
+    closeDelay?: number;
 }
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Trigger
+ * -----------------------------------------------------------------------------------------------*/
+export const Trigger: React.ForwardRefExoticComponent<HoverCardTriggerProps & React.RefAttributes<HTMLAnchorElement>>;
+
+interface HoverCardTriggerProps extends PrimitiveLinkProps {
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Trigger
+ * -----------------------------------------------------------------------------------------------*/
+export const Portal: React.FC<HoverCardPortalProps>;
+
+interface HoverCardPortalProps {
+    children?: React.ReactNode;
+    /**
+     * Specify a container element to portal the content into.
+     */
+    container?: PortalProps['container'];
+    /**
+     * Used to force mounting when more control is needed. Useful when
+     * controlling animation with React animation libraries.
+     */
+    forceMount?: true;
+}
+
+
+/* -------------------------------------------------------------------------------------------------
+ * Content
+ * -----------------------------------------------------------------------------------------------*/
+export const Content: React.ForwardRefExoticComponent<HoverCardContentProps & React.RefAttributes<HTMLDivElement>>;
 
 interface HoverCardContentProps extends HoverCardContentImplProps {
-  /**
-   * Used to force mounting when more control is needed. Useful when
-   * controlling animation with React animation libraries.
-   */
-  forceMount?: true;
+    /**
+     * Used to force mounting when more control is needed. Useful when
+     * controlling animation with React animation libraries.
+     */
+    forceMount?: true;
+}
+interface HoverCardContentImplProps extends Omit<PopperContentProps, 'onPlaced'> {
+    /**
+     * Event handler called when the escape key is down.
+     * Can be prevented.
+     */
+    onEscapeKeyDown?: DismissableLayerProps['onEscapeKeyDown'];
+    /**
+     * Event handler called when the a `pointerdown` event happens outside of the `HoverCard`.
+     * Can be prevented.
+     */
+    onPointerDownOutside?: DismissableLayerProps['onPointerDownOutside'];
+    /**
+     * Event handler called when the focus moves outside of the `HoverCard`.
+     * Can be prevented.
+     */
+    onFocusOutside?: DismissableLayerProps['onFocusOutside'];
+    /**
+     * Event handler called when an interaction happens outside the `HoverCard`.
+     * Specifically, when a `pointerdown` event happens outside or focus moves outside of it.
+     * Can be prevented.
+     */
+    onInteractOutside?: DismissableLayerProps['onInteractOutside'];
 }
 
-export const Content: React.ForwardRefExoticComponent<
-  HoverCardContentProps & React.RefAttributes<HTMLDivElement>
->;
+
+/* -------------------------------------------------------------------------------------------------
+ * Arrow
+ * -----------------------------------------------------------------------------------------------*/
+export const Arrow: React.ForwardRefExoticComponent<HoverCardArrowProps & React.RefAttributes<SVGSVGElement>>;
+
+interface HoverCardArrowProps extends PopperArrowProps {
+}
