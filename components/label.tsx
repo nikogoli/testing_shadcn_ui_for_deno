1ade4d5b5bb1ea787ab0fe47cab2591f68e22b33
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-25 17:11:42 +0900

    変更(Label)：型 調整

--- components/label.tsx
+++ components/label.tsx
@@ -1,15 +1,16 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
+// @deno-types="../modules/lib/components/label.d.ts"
 import * as LabelPrimitive from '../modules/esm.sh/v132/_@radix-ui/react-label@2.0.2.js'
-import * as AltLabelPrimitive from "../modules/lib/components/label.d.ts"
+import * as LabelTypes from "../modules/lib/components/label.d.ts"
 
 import { cn } from '../modules/lib/utils.ts'
 import { ElementRef, ComponentPropsWithoutRef } from "../modules/lib/type-utils.ts"
 
 
 const Label = React.forwardRef<
-  ElementRef<typeof AltLabelPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof AltLabelPrimitive.Root>
+  ElementRef<typeof LabelTypes.Root>,
+  ComponentPropsWithoutRef<typeof LabelTypes.Root>
 >(({ class:className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
--- modules/lib/components/label.d.ts
+++ modules/lib/components/label.d.ts
@@ -1,14 +1,15 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../../esm.sh/preact@10.19.6/compat.js'
-import { PrimitiveForwardRefComponent, ComponentPropsWithoutRef } from "../type-utils.ts"
 
-/**
- * Following type-definitions are based on "https://esm.sh/v132/@radix-ui/react-label@2.0.2/X-YS9AdHlwZXMvcmVhY3Q6cHJlYWN0L2NvbXBhdCxyZWFjdDpwcmVhY3QvY29tcGF0CmUvKg/dist/index.d.mts"
- */ 
+import { PrimitiveLabelProps } from "../type-utils-Primitive.d.ts"
 
-type PrimitiveLabelProps = ComponentPropsWithoutRef<PrimitiveForwardRefComponent<"label">>;
-export interface LabelProps extends PrimitiveLabelProps {
-}
+
+/* -------------------------------------------------------------------------------------------------
+ * Root
+ * -----------------------------------------------------------------------------------------------*/
 export const Root: React.ForwardRefExoticComponent<
   LabelProps & React.RefAttributes<HTMLLabelElement>
 >;
+
+interface LabelProps extends PrimitiveLabelProps {
+}
