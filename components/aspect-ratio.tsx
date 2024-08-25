fbc97318b35566d926ec5982baefeb7f55399e52
nikogoli <k_m_naoto@yahoo.co.jp>
2024-08-25 13:46:04 +0900

    変更(AspctRto)：型 調整

--- components/aspect-ratio.tsx
+++ components/aspect-ratio.tsx
@@ -1,14 +1,14 @@
 // @deno-types="https://esm.sh/v128/preact@10.19.6/compat/src/index.d.ts"
 import * as React from '../modules/esm.sh/preact@10.19.6/compat.js'
-import { ComponentPropsWithoutRef, PrimitiveForwardRefComponent } from "../modules/lib/type-utils.ts"
+import { PrimitiveDivProps } from "../modules/lib/type-utils-Primitive.d.ts"
 import * as AspectRatioPrimitive from "../modules/esm.sh/_@radix-ui/react-aspect-ratio@1.0.3.js"
 
 
-type PrimitiveDivProps = ComponentPropsWithoutRef<PrimitiveForwardRefComponent<"div">>
 interface AspectRatioProps extends PrimitiveDivProps {
     ratio?: number
 }
 
-const AspectRatio:React.ForwardRefExoticComponent<AspectRatioProps & React.RefAttributes<HTMLDivElement>> = AspectRatioPrimitive.Root
 
-export { AspectRatio }
+export const AspectRatio: React.ForwardRefExoticComponent<
+    AspectRatioProps & React.RefAttributes<HTMLDivElement>
+> = AspectRatioPrimitive.Root
